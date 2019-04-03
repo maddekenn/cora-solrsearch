d3bf5b93dcd5d9663ce0cb7b8ccc9b57aacc0ee3
olovm <olov.mckie@ub.uu.se>
2019-04-03 07:59:11 +0000

    updating for use of java modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module se.uu.ub.cora.solrsearch {
+	requires transitive se.uu.ub.cora.searchstorage;
+	requires transitive se.uu.ub.cora.spider;
+	requires transitive solr.solrj;
+
+	exports se.uu.ub.cora.solr;
+	exports se.uu.ub.cora.solrindex;
+	exports se.uu.ub.cora.solrsearch;
+}
